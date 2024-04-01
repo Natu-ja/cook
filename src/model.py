51674325fc808da91bd9f25d612149dea00033ca
Natu-ja <107454696+Natu-ja@users.noreply.github.com>
2024-04-01 12:37:32 +0000

    fixed

--- src/model.py
+++ src/model.py
@@ -1,7 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def load(args):
+
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    print(f'Loaded tokenizer from {args.tokenizer}, vocab size {tokenizer.vocab_size}')
 
     model = AutoModelForCausalLM.from_pretrained(args.model, trust_remote_code=True)
     print(f'Loaded model from {args.model}, model size {model.num_parameters()}')
